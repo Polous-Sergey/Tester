b6105fb0bc8ca7c6709d8a8b0c221a2af0c7a0bb
Polous-Sergey <polous@ro.ru>
2018-05-09 13:34:14 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MainUserComponent} from './main-user/main-user.component';
+import {MainAdminComponent} from './main-admin/main-admin.component';
+import {UserListComponent} from './main-admin/user-list/user-list.component';
+import {SurveyListComponent} from './main-admin/survey-list/survey-list.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'user', component: MainUserComponent},
+    {path: 'admin', component: MainAdminComponent, children: [
+            {path: 'user', component: UserListComponent},
+            {path: 'survey', component: SurveyListComponent},
+        ]},
+    {path: '**', redirectTo: '/login'}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}

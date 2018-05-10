1cc419988477780732bd7018c9ba1c6228e93f38
Polous-Sergey <polous@ro.ru>
2018-05-11 01:51:44 +0300

    admin user management

--- src/app/main-admin/user-list/user-list.component.ts
+++ src/app/main-admin/user-list/user-list.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {User} from '../../shared/model/user';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+    displayedColumns = ['name', 'surname', 'email', 'progress', 'assessment', 'actions'];
+    dataSource: MatTableDataSource<User>;
+    users: User[] = [
+        {
+            name: 'name',
+            surname: 'surname',
+            email: 'email',
+            progress: false,
+            assessment: 5
+        },
+        {
+            name: 'name2',
+            surname: 'surname2',
+            email: 'email2',
+            progress: true,
+            assessment: 4
+        }
+    ];
 
-  constructor() { }
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.dataSource = new MatTableDataSource(this.users);
+    }
+
+    ngOnInit() {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+    }
+
+    applyFilter(filterValue: string) {
+        filterValue = filterValue.trim();
+        filterValue = filterValue.toLowerCase();
+        this.dataSource.filter = filterValue;
+    }
 
 }
